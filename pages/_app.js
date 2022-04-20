5f359ac074d2aec16ba8c51cad5e5a943078f1f1
juiloallendes <julioallendes99@gmail.com>
2022-04-20 19:33:54 -0400

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
+
+  const theme = createTheme({
+    type: "dark",
+    theme: {
+      colors: {
+        // brand colors
+        primary: '#7928ca',
+
+        link: '#F74C77',
+        
+      },
+      space: {},
+      fonts: {
+        sans: "Lato"
+      }
+    }
+  })
+
+
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Component {...pageProps} />
     </NextUIProvider>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Avatar, Grid } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Julio Allendes | Programador web </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <Grid.Container gap={5}>
+        <Grid>
+          <Avatar 
+            src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png"
+            css={{ size: '$20'}}
+          />
+        </Grid>
+      </Grid.Container>
+      <main>
+        <h1>Julio Allendes</h1>
+        
+      </main>
+
     </div>
   )
 }
